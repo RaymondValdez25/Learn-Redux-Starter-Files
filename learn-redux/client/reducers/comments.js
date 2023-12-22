32e42b6fe5117b719815fe414da4190f3e9f1cbd
Raymond Valdez <Raymondvaldez25@gmail.com>
2023-12-22 14:09:14 -0800

    finished vid 7

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,12 @@
+//a reducer takes in twp things:
+
+//1. the action (info about what happened)
+//2. a copy of current state
+
+
+function comments(state = [], action){
+    console.log(state, action);
+    return state
+}
+
+export default posts;
