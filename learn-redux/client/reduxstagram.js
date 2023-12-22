04943cec1d4dbcab647254009d81cc87bb06c7de
Raymond Valdez <Raymondvaldez25@gmail.com>
2023-12-22 12:20:07 -0800

    finished vid 4

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 //import css
 import css from'./styles/style.styl'
 //Import Components
 import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
-render(<Main><p>Hello</p></Main>, document.getElementById('root'));
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:PostId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'));
